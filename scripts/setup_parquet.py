4368cb3a77cb4017a9d64d2373ecd4e0d3fb0c96
Matthew Deakos <mwoolnerdeakos@gmail.com>
2023-09-30 21:33:47 -0300

    BLAZINGLY FAST

--- scripts/setup_parquet.py
+++ scripts/setup_parquet.py
@@ -1,5 +1,7 @@
 import argparse
 import os
+from dataclasses import dataclass
+from multiprocessing import Pool, cpu_count
 from typing import Literal
 
 from tqdm.auto import tqdm
@@ -19,25 +21,51 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
+@dataclass
+class WorkerArgs:
+    src_path: str
+    outpath: str
+    files: list[str]
+    quiet: bool
+
+
+def process_files(args: WorkerArgs) -> None:
+    writer = ParquetWriter(args.src_path, args.outpath)
+
+    for file in tqdm(args.files, disable=args.quiet):
+        writer.process_file(file)
+
+
+def chunk_files(files: list[str], num_chunks: int):
+    chunk_size = len(files) // num_chunks
+    return [files[i : i + chunk_size] for i in range(0, len(files), chunk_size)]
+
+
 def setup_parquet(
     directory: str,
-    splits: list[Split] = [
-        "train",
-        "valid",
-        "test",
-    ],
+    splits: list[str] = ["train", "valid", "test"],
     quiet: bool = True,
 ) -> None:
+    num_processes = min(
+        cpu_count(), len(splits)
+    )  # Ensure we don't create unnecessary processes
+
     for split in splits:
         if not quiet:
             print("Processing", split)
+
         src_path = os.path.join(directory, split)
         outpath = os.path.join("data/parquet/", split)
-        writer = ParquetWriter(src_path, outpath)
         files = os.listdir(src_path)
 
-        for file in tqdm(files, disable=quiet):
-            writer.process_file(file)
+        with Pool(processes=num_processes) as pool:
+            pool.map(
+                process_files,
+                [
+                    WorkerArgs(src_path, outpath, chunk, quiet)
+                    for chunk in chunk_files(files, num_processes)
+                ],
+            )
 
 
 def main() -> None:
