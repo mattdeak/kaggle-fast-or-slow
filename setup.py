2c625bbc353dbae3e93ea25bfeb2ec0ab0c737b8
Matthew Deakos <mwoolnerdeakos@gmail.com>
2023-10-02 01:01:26 -0300

    unfuck setup

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ setup(
     packages=find_packages(),
     version="0.1.0",
     description="Kaggle Fast and Slow | Model RunTime Prediction",
-    console_scripts=["setup-parquet=kaggle_fs.scripts:main"],
+    entry_points={
+        "console_scripts": ["setup-parquet=scripts.setup_parquet:main"],
+    },
 )
