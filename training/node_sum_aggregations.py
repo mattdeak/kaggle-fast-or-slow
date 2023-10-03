022706867f521f4c30f0822eef6128b7387a18b3
Matthew Deakos <mwoolnerdeakos@gmail.com>
2023-10-03 15:08:33 -0300

    sweep config fix

--- training/node_sum_aggregations.py
+++ training/node_sum_aggregations.py
@@ -16,13 +16,13 @@ sweep_configuration = {
     "name": "sweep",
     "metric": {"goal": "maximize", "name": "val_perf"},
     "parameters": {
-        "learning_rate": [0.0001, 0.001, 0.01, 0.05, 0.1],
-        "max_depth": np.arange(3, 14, 1).tolist(),
-        "min_child_weight": np.arange(1, 6, 1).tolist(),
-        "subsample": np.arange(0.5, 1.0, 0.1).tolist(),
-        "colsample_bytree": np.arange(0.5, 1.0, 0.1).tolist(),
-        "gamma": [0, 0.1, 0.2, 0.3, 0.4, 0.5],
-        "n_estimators": [100, 200, 300, 400, 500],
+        "learning_rate": {"values": [0.0001, 0.001, 0.01, 0.05, 0.1]},
+        "max_depth": {"values": np.arange(3, 14, 1).tolist()},
+        "min_child_weight": {"values": np.arange(1, 6, 1).tolist()},
+        "subsample": {"values": np.arange(0.5, 1.0, 0.1).tolist()},
+        "colsample_bytree": {"values": np.arange(0.5, 1.0, 0.1).tolist()},
+        "gamma": {"values": [0, 0.1, 0.2, 0.3, 0.4, 0.5]},
+        "n_estimators": {"values": [100, 200, 300, 400, 500]},
     },
 }
 
