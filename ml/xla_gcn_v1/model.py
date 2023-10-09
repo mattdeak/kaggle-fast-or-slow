d503d8eec5007a6d6634ceb9e4d6b2ca44deba34
Matthew Deakos <mwoolnerdeakos@gmail.com>
2023-10-09 20:02:56 -0300

    ok

--- ml/xla_gcn_v1/model.py
+++ ml/xla_gcn_v1/model.py
@@ -1,8 +1,7 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.data import Data, DataLoader, Dataset, InMemoryDataset
+from torch_geometric.data import Data
 from torch_geometric.nn import GCNConv, global_mean_pool
-from tqdm.auto import tqdm
 
 
 class ModifiedGCN(torch.nn.Module):
@@ -10,15 +9,28 @@ class ModifiedGCN(torch.nn.Module):
         self,
         graph_input_dim: int,
         global_input_dim: int,
-        hidden_dim: int,
+        gcn_out_dims: list[int],
+        linear_dims: list[int],
         output_dim: int,
     ):
         super().__init__()
-        self.conv1 = GCNConv(graph_input_dim, hidden_dim)
-        self.conv2 = GCNConv(hidden_dim, 8)
 
-        self.fc1 = torch.nn.Linear(global_input_dim + 8, 16)
-        self.fc2 = torch.nn.Linear(16, output_dim)
+        initial_conv = GCNConv(graph_input_dim, gcn_out_dims[0])
+        self.convs = torch.nn.ModuleList(
+            [initial_conv]
+            + [GCNConv(i, o) for i, o in zip(gcn_out_dims[:-1], gcn_out_dims[1:])]
+        )
+
+        first_linear = torch.nn.Linear(
+            gcn_out_dims[-1] + global_input_dim, linear_dims[0]
+        )
+
+        self.fcs = torch.nn.ModuleList(
+            [first_linear]
+            + [torch.nn.Linear(i, o) for i, o in zip(linear_dims[:-1], linear_dims[1:])]
+        )
+
+        self.output_layer = torch.nn.Linear(linear_dims[-1], output_dim)
 
     def forward(self, data: Data):
         x, edge_index, global_features = (
@@ -26,19 +38,17 @@ class ModifiedGCN(torch.nn.Module):
             data.edge_index,
             data["global_features"],
         )
-        x = self.conv1(x, edge_index)
-        x = F.leaky_relu(x)
 
-        x = self.conv2(x, edge_index)
-        x = F.leaky_relu(x)
+        for conv in self.convs:
+            x = conv(x, edge_index)
+            x = F.leaky_relu(x)
 
         pool = global_mean_pool(x, data.batch)
-        concat = torch.cat((pool, global_features), dim=1)
+        x = torch.cat((pool, global_features), dim=1)
 
-        x = self.fc1(concat)
+        for fc in self.fcs:
+            x = fc(x)
+            x = F.leaky_relu(x)
 
-        x = F.leaky_relu(x)
-        x = self.fc2(x)
-
-        x = F.leaky_relu(x)
+        x = self.output_layer(x)
         return x
