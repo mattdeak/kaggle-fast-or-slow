44557926bbb76bcbb683128ebc41599c022fdd5d
Matthew Deakos <mwoolnerdeakos@gmail.com>
2023-10-25 14:34:04 -0300

    increase batch size

--- ml/layout_v1/train.py
+++ ml/layout_v1/train.py
@@ -50,7 +50,7 @@ POOLING_RATIO = None
 
 
 # Training Details
-BATCH_SIZE = 2  # pretty low cause memory is hard
+BATCH_SIZE = 4  # pretty low cause memory is hard
 NUM_WORKERS = 4
 XLA_DATA_DIR = "data/layout/xla"
 NLP_DATA_DIR = "data/layout/nlp"
