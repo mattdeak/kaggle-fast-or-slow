047e79389d2c9104ac6bf6fe1dac0f2b52397b19
Matthew Deakos <mwoolnerdeakos@gmail.com>
2023-11-01 17:14:54 -0300

    forgot to delete that

--- ml/layout_v1/preprocessors.py
+++ ml/layout_v1/preprocessors.py
@@ -34,12 +34,4 @@ def reduce_to_config_node_communities_ndarray(
         new_node_config_ids[new_node_config_ids == n] = i
 
     x = x[kept_nodes, :]
-
-    # we also need to set new node config ids. they need to map to the same
-    # node config ids as before, but now they are reduced to the new node ids
-    # so we need to map the old node config ids to the new node ids
-    # we can do this by using the old node config ids as indices into the
-    # kept_nodes array
-    node_config_ids = kept_nodes[node_config_ids]
-
     return x, new_edge_index, node_config_ids
