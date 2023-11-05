a8361567aa7800f0fd228bf06c1ec8edfb47f72a
Matthew Deakos <mwoolnerdeakos@gmail.com>
2023-11-05 11:19:31 -0400

    try more pooling

--- ml/layout_v1/model.py
+++ ml/layout_v1/model.py
@@ -19,19 +19,12 @@ class SAGEBlock(nn.Module):
         output_dim: int,
         with_residual: bool = True,
         dropout: float = 0.5,
-        pooling_ratio: float | None = None,
     ):
         super().__init__()
         self.conv = SAGEConv(input_dim, output_dim)
         self.norm = nn.LayerNorm(output_dim)
         self.with_residual = with_residual
         self.dropout = nn.Dropout(dropout)
-
-        self.pooling_layer = (
-            SAGPooling(input_dim, ratio=pooling_ratio, dropout=dropout)
-            if pooling_ratio
-            else None
-        )
 
         self.output_dim = output_dim
 
@@ -42,11 +35,6 @@ class SAGEBlock(nn.Module):
         f = F.gelu(f)
         f = self.norm(f)
         f = self.dropout(f)
-
-        if self.pooling_layer:
-            f, edge_index, _, batch, _, _ = self.pooling_layer(
-                f, edge_index, batch=batch
-            )
 
         if self.with_residual:
             f += x
@@ -111,14 +99,13 @@ class SAGEMLP(nn.Module):
                 output_dim=sage_channels,
                 dropout=dropout,
                 with_residual=False,  # doesn't play well with pooling
-                pooling_ratio=pooling_ratio,
             )
             self.gcns.append(block)
 
         if global_features_dim:
-            first_linear_input_dim = block.output_dim + global_features_dim
+            first_linear_input_dim = 3 * block.output_dim + global_features_dim
         else:
-            first_linear_input_dim = block.output_dim
+            first_linear_input_dim = 3 * block.output_dim
 
         self.mlp = nn.Sequential(
             LinearBlock(
@@ -139,7 +126,11 @@ class SAGEMLP(nn.Module):
         for gcn_block in self.gcns:
             d = gcn_block(d)
 
-        pool = global_mean_pool(d.x, d.batch)
+        mean_pool = global_mean_pool(d.x, d.batch)
+        max_pool = global_max_pool(d.x, d.batch)
+        min_pool = global_max_pool(-d.x, d.batch)
+
+        pool = torch.cat([mean_pool, max_pool, min_pool], dim=1)
 
         if data.global_features is not None:
             # shape we need from global features is (batch, global_features_dim)
