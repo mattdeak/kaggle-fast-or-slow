914ae183f9984eeca9f28bcbad5c2b9d24cb340b
Matthew Deakos <mwoolnerdeakos@gmail.com>
2023-11-06 15:22:31 -0400

    update imports

--- ml/layout_v1/preprocessors/__init__.py
+++ ml/layout_v1/preprocessors/__init__.py
@@ -3,4 +3,5 @@ from ml.layout_v1.preprocessors.global_preproc import GlobalFeatureGenerator
 from ml.layout_v1.preprocessors.graph_preproc import \
     ConfigNodeCommunityPreprocessor
 from ml.layout_v1.preprocessors.node_preproc import NodePreprocessor
-from ml.layout_v1.preprocessors.opcode_preproc import OpcodeGroupOHEEmbedder
+from ml.layout_v1.preprocessors.opcode_preproc import (OpcodeGroupOHEEmbedder,
+                                                       ohe_opcodes)
