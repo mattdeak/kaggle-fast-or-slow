ca64a2af5d67cc4be26e4a2688ce041cbab09f98
Matthew Deakos <mwoolnerdeakos@gmail.com>
2023-11-08 01:21:10 -0400

    add epsilon to log

--- ml/layout_v1/preprocessors/node_preproc.py
+++ ml/layout_v1/preprocessors/node_preproc.py
@@ -31,7 +31,7 @@ class NodeProcessor:
         # Engineered features
         engineered = np.hstack(
             (
-                np.log(self.calculate_shape_sparsity(x)),
+                np.log(self.calculate_shape_sparsity(x) + EPSILON),
                 self.dimensionality(x),
                 self.stride_interactions(x),
                 self.padding_proportions(x),
