09ed8c5eca9a690f4040663c9fcae479c42fb6e8
Matthew Deakos <mwoolnerdeakos@gmail.com>
2023-11-10 14:08:10 -0400

    type fixes

--- ml/layout_v1/preprocessors/graph_preproc.py
+++ ml/layout_v1/preprocessors/graph_preproc.py
@@ -66,8 +66,10 @@ class GraphProcessor:
 
         # Remap the nodes in the edge index.
         node_mapping = self.remapper(edge_index, node_config_ids)
-        new_edge_index = remap_edges(edge_index, node_mapping)
-        alternate_edge_index = remap_edges(alternate_edge_index, node_mapping)
+        new_edge_index, _ = remap_edges(edge_index, node_mapping)
+        alternate_edge_index, alternate_edge_index_attr = remap_edges(
+            alternate_edge_index, node_mapping, alternate_edge_index_attr
+        )
         node_features, opcodes = remap_nodes(node_features, opcodes, node_mapping)
 
         return GraphTransformReturnType(
@@ -179,8 +181,10 @@ def remap_nodes(
 
 
 def remap_edges(
-    edge_index: npt.NDArray[np.int_], node_mapping: npt.NDArray[np.int_]
-) -> npt.NDArray[np.int_]:
+    edge_index: npt.NDArray[np.int_],
+    node_mapping: npt.NDArray[np.int_],
+    edge_index_attr: npt.NDArray[np.float_] | None = None,
+) -> tuple[npt.NDArray[np.int_], npt.NDArray[np.float_] | None]:
     """Remaps the edge index to the new node ids.
     Args:
         edge_index: The edge index in COO (e x 2)
@@ -191,13 +195,17 @@ def remap_edges(
     """
     # We can only remap the edge index if both nodes are in the node mapping.
     # Otherwise, we remove the edge.
-    new_edge_index = edge_index[
-        np.isin(edge_index[:, 0], node_mapping)
-        & np.isin(edge_index[:, 1], node_mapping),
-        :,
-    ]
+    ix = np.isin(edge_index[:, 0], node_mapping) & np.isin(
+        edge_index[:, 1], node_mapping
+    )
+
+    new_edge_index = edge_index[ix, :]
+
+    new_edge_index_attr = None
+    if edge_index_attr is not None:
+        new_edge_index_attr = edge_index_attr[ix:,]
 
     for i, n in enumerate(node_mapping):
         new_edge_index[new_edge_index == n] = i
 
-    return new_edge_index
+    return new_edge_index, new_edge_index_attr
