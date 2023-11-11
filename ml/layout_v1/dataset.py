88e294756cdc28417e80122ef0c2d18dd4244583
Matthew Deakos <mwoolnerdeakos@gmail.com>
2023-11-10 23:04:34 -0400

    ok idea

--- ml/layout_v1/dataset.py
+++ ml/layout_v1/dataset.py
@@ -135,6 +135,7 @@ class LayoutDataset(Dataset):
         progress: bool = True,
         multiprocess: bool = True,
         max_workers: int = 3,  # has to be pretty low, memory intensive
+        collate_fn: Any | None = None,
     ):
         """Directories should be a list of directories to load from.
 
@@ -178,7 +179,7 @@ class LayoutDataset(Dataset):
 
         self.load()
 
-        super().__init__()
+        super().__init__(collate_fn=collate_fn)
 
     def len(self):
         return len(self.idx_to_config)
