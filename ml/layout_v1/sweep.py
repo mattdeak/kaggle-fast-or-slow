707124d58b2d686c86edb1708479b1d5638c8f24
Matthew Deakos <mwoolnerdeakos@gmail.com>
2023-11-12 16:22:44 -0400

    sweep definitions

--- ml/layout_v1/sweep.py
+++ ml/layout_v1/sweep.py
@@ -3,8 +3,9 @@ import wandb
 # from ml.layout_v1.train import DEFAULT_CONFIG, run
 
 SWEEP_CONFIG_XLA_ONLY = {
+    "program": "train.py",
     "method": "random",
-    "name": "hp-sweep",
+    "name": "hp-sweep-xla-only",
     "metric": {"name": "full/avg/kendall_tau", "goal": "maximize"},
     "parameters": {
         "dataset_types": {"value": ["xla"]},
@@ -51,12 +52,116 @@ SWEEP_CONFIG_XLA_ONLY = {
     },
 }
 
+SWEEP_CONFIG_NLP_ONLY = {
+    "program": "train.py",
+    "method": "random",
+    "name": "hp-sweep-nlp-only",
+    "metric": {"name": "full/avg/kendall_tau", "goal": "maximize"},
+    "parameters": {
+        "dataset_types": {"value": ["nlp"]},
+        "dataset_subtypes": {"value": ["default", "random"]},
+        "graph_layers": {"values": [3, 4, 5]},
+        "graph_channels": {"values": [64, 128, 256]},
+        "linear_layers": {"values": [2, 3, 4, 5]},
+        "linear_channels": {"values": [64, 128, 256]},
+        "dropout": {"values": [0.0, 0.05, 0.1, 0.2]},
+        "pooling": {"values": ["mean", "max", "multi"]},
+        "graph_convolution_type": {"values": ["sage", "gat"]},
+        "graph_convolution_kwargs": {
+            "parameters": {
+                "heads": {"values": [1, 2, 4, 8]},
+            },
+        },
+        "graph_norm": {"values": ["graph", "layer"]},
+        "linear_norm": {"values": ["layer", "batch"]},
+        "optimizer": {"value": ["adamw"]},
+        "optimizer_kwargs": {
+            "parameters": {
+                "lr": {"max": 5e-3, "min": 1e-4},
+                "weight_decay": {"max": 0.01, "min": 0.0, "distribution": "uniform"},
+            },
+        },
+        "epochs": {"values": [6]},
+        "batch_size": {"values": [8, 16]},
+        "preprocessors": {
+            "parameters": {
+                "global_kwargs": {
+                    "parameters": {
+                        "subtype_indicator": {"values": [True, False]},
+                    },
+                },
+            }
+        },
+        "crossover": {"min": 0.0, "max": 0.2},
+        "use_distribution_flag": {"values": [True, False]},
+        "use_multi_edge": {"values": [True, False]},
+        "criterion": {"values": ["listMLE", "margin-loss"]},
+        "criterion_kwargs": {
+            "parameters": {"margin": {"min": 0.5, "max": 3.0}},
+        },
+    },
+}
 
-def define_sweep() -> str:
-    sweep_id = wandb.sweep(SWEEP_CONFIG_XLA_ONLY, project="kaggle-fast-or-slow")
-    return sweep_id
+SWEEP_CONFIG_ALL_DATA = {
+    "program": "train.py",
+    "method": "random",
+    "name": "hp-sweep-nlp-only",
+    "metric": {"name": "full/avg/kendall_tau", "goal": "maximize"},
+    "parameters": {
+        "dataset_types": {"value": ["nlp", "xla"]},
+        "dataset_subtypes": {"value": ["default", "random"]},
+        "graph_layers": {"values": [3, 4, 5]},
+        "graph_channels": {"values": [64, 128, 256]},
+        "linear_layers": {"values": [2, 3, 4, 5]},
+        "linear_channels": {"values": [64, 128, 256]},
+        "dropout": {"values": [0.0, 0.05, 0.1, 0.2]},
+        "pooling": {"values": ["mean", "max", "multi"]},
+        "graph_convolution_type": {"values": ["sage", "gat"]},
+        "graph_convolution_kwargs": {
+            "parameters": {
+                "heads": {"values": [1, 2, 4, 8]},
+            },
+        },
+        "graph_norm": {"values": ["graph", "layer"]},
+        "linear_norm": {"values": ["layer", "batch"]},
+        "optimizer": {"value": ["adamw"]},
+        "optimizer_kwargs": {
+            "parameters": {
+                "lr": {"max": 5e-3, "min": 1e-4},
+                "weight_decay": {"max": 0.01, "min": 0.0, "distribution": "uniform"},
+            },
+        },
+        "epochs": {"values": [6]},
+        "batch_size": {"values": [8, 16]},
+        "preprocessors": {
+            "parameters": {
+                "global_kwargs": {
+                    "parameters": {
+                        "subtype_indicator": {"values": [True, False]},
+                    },
+                },
+            }
+        },
+        "crossover": {"min": 0.0, "max": 0.2},
+        "use_distribution_flag": {"values": [True, False]},
+        "use_multi_edge": {"values": [True, False]},
+        "criterion": {"values": ["listMLE", "margin-loss"]},
+        "criterion_kwargs": {
+            "parameters": {"margin": {"min": 0.5, "max": 3.0}},
+        },
+    },
+}
+
+
+def define_sweeps() -> None:
+    sweep_id1 = wandb.sweep(SWEEP_CONFIG_XLA_ONLY, project="kaggle-fast-or-slow")
+    sweep_id2 = wandb.sweep(SWEEP_CONFIG_NLP_ONLY, project="kaggle-fast-or-slow")
+    sweep_id3 = wandb.sweep(SWEEP_CONFIG_ALL_DATA, project="kaggle-fast-or-slow")
+
+    print(f"XLA-only sweep: {sweep_id1}")
+    print(f"NLP-only sweep: {sweep_id2}")
+    print(f"All-data sweep: {sweep_id3}")
 
 
 if __name__ == "__main__":
-    id_ = define_sweep()
-    print(id_)
+    define_sweep()
